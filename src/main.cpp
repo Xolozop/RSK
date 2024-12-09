3cf083a6e1b044f20a6edde3b781d67d46ab9c96
Xolozop <50333438+Xolozop@users.noreply.github.com>
2024-12-09 19:03:05 +0300

    upd interface for geometricalRSK

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,13 @@
 
 #include "types.h"
 #include "classical.h"
+#include "geometrical.h"
+
+// 1 8 7 9 6 2 10 1 3 4 5
 
 Permutation getRandomPermutation(const Permutation& perm, const size_t n) {
     Permutation newPerm = perm;
-    srand(time(0));
+    //srand(time(0));
     for (size_t i = n-1; i > 0; i--) {
         size_t j = std::rand()%i;
         size_t c = newPerm[j];
@@ -18,39 +21,19 @@ Permutation getRandomPermutation(const Permutation& perm, const size_t n) {
     return newPerm;
 }
 
-int main() {
-    /* создаём случайную пеерстановку */
-    Permutation permutation;
-    size_t n = 100000;
-    for (size_t i = 0; i < n; i++)
-        permutation.emplace_back(i+1);
-    Permutation permutation2 = getRandomPermutation(permutation, n);
-    //Permutation permutation2 = {3, 1, 6, 10, 2, 5, 8, 4, 9, 7}; // для примера Вьенно
-    //Permutation permutation2 = {5, 8, 1, 9, 6, 4, 10, 3, 7, 2}; // для примера
-
-    /* классичесский RSK + вычисление времени */
-    clock_t start = clock();
-    std::pair<Tableau, Tableau> ans = classicalRSK(permutation2);
-    clock_t end = clock();
-    double classical_seconds = (double)(end - start)/CLOCKS_PER_SEC;
-
-    std::cout << "Затрачено времени на классический RSK: " << std::fixed << classical_seconds << " секунд" << std::endl;
-
-    /* запись в файл результата */
-    if (n > 1000)
-        return 0;
+void writeToFile(Permutation permutation, std::pair<Tableau, Tableau> ans, std::string filename) {
     std::ofstream out;
-    out.open("result.txt");
+    out.open(filename);
     if (!out.is_open()) {
         out.close();
-        return 0;
+        return;
     }
     out << "Permutation:" << std::endl << '\t';
-    for (size_t i = 0; i < n; i++)
-        out << permutation[i] << ' ';
+    for (size_t i = 0; i < permutation.size(); i++)
+        out << i+1 << ' ';
     out << std::endl << '\t';
-    for (size_t i = 0; i < n; i++)
-        out << permutation2[i] << ' ';
+    for (size_t i = 0; i < permutation.size(); i++)
+        out << permutation[i] << ' ';
     out << std::endl;
 
     Tableau P = ans.first;
@@ -72,5 +55,68 @@ int main() {
         out << std::endl;
     }
     out.close();
+}
+
+int main() {
+    /* создаём случайную пеерстановку */
+    Permutation base;
+    size_t n = 10;
+    for (size_t i = 0; i < n; i++)
+        base.emplace_back(i+1);
+    //Permutation permutation = getRandomPermutation(base, n);
+    Permutation permutation = {3, 1, 6, 10, 2, 5, 8, 4, 9, 7}; // для примера Вьенно
+    //Permutation permutation = {6, 1, 7, 2, 5, 8, 3, 9, 4}; // для геометрического примера Вьенно
+    //Permutation permutation = {5, 8, 1, 9, 6, 4, 10, 3, 7, 2}; // для примера
+    //Permutation permutation = {8, 7, 9, 6, 2, 10, 1, 3, 4, 5};
+
+    /* классичесский RSK + вычисление времени */
+    clock_t start, end;
+    start = clock();
+    std::pair<Tableau, Tableau> classicalAns = classicalRSK(permutation);
+    end = clock();
+    double classicalSeconds = (double)(end - start)/CLOCKS_PER_SEC;
+
+    /* геометрическая конструкция Вьенно + вычисление времени */
+    start = clock();
+    std::pair<Tableau, Tableau> geometricalAns = geometricalRSK(permutation);
+    end = clock();
+    double geometricalSeconds = (double)(end - start)/CLOCKS_PER_SEC;
+
+    std::cout << "Затрачено времени на классический RSK: " << std::fixed << classicalSeconds << " секунд" << std::endl;
+    std::cout << "Затрачено времени на геометрический RSK: " << std::fixed << geometricalSeconds << " секунд" << std::endl;
+    
+    /* запись в файл результата */
+    if (n < 1000) {
+        writeToFile(permutation, classicalAns, "results/classical.txt");
+        writeToFile(permutation, geometricalAns, "results/geometrical.txt");
+    }
+
+    bool equivalence = true;
+    for (int x = classicalAns.first.size()-1; x >= 0; x--) {
+        for (int y = 0; y < classicalAns.first[x].size(); y++) {
+            if (classicalAns.first[x][y] != geometricalAns.first[x][y]) {
+                equivalence = false;
+                break;
+            }
+        }
+        if (!equivalence)
+            break;
+    }
+
+    for (int x = classicalAns.second.size()-1; x >= 0; x--) {
+        for (int y = 0; y < classicalAns.second[x].size(); y++) {
+            if (classicalAns.second[x][y] != geometricalAns.second[x][y]) {
+                equivalence = false;
+                break;
+            }
+        }
+        if (!equivalence)
+            break;
+    }
+
+    std::cout << "\t~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "Полученные разными способами таблицы Юнга ";
+    std::cout << (equivalence ? "эквивалентны!" : "не эквивалентны...") << std::endl;
+
     return 0;
 }
